92e5e1090b318683e9a2bb7f8a26671111aa550f
czjczj <2838588360@qq.com>
2019-03-04 16:46:17 +0800

    测试

---
+++ LeetCoding/src/DP/Test.java
@@ -0,0 +1,12 @@
+package DP;
+
+/**
+ * @author czj
+ * @date   2019-03-04 16:27
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("520");
+		System.out.println("5201313");
+	}
+}
